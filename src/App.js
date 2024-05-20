0466e416f439aa874dac1762f2d1d8fd37dbca23
AbdullahNasser2005 <166999175+AbdullahNasser2005@users.noreply.github.com>
2024-05-21 00:41:54 +0300

    Describe your changes here

--- src/App.js
+++ src/App.js
@@ -2,29 +2,34 @@ import { useRef, useState } from 'react';
 import "./App.css";
 
 function App() {
-  const [toDoes, setToDoes] = useState([])
-  const inputRef = useRef()
+  const [toDoes, setToDoes] = useState([]);
+  const inputRef = useRef();
 
-  const handlare = ()=>{
+  const handleAdd = () => {
     const text = inputRef.current.value;
-    setToDoes([...toDoes,text])
-    inputRef.current.value = ""
-  }
+    if (text.trim()) {
+      setToDoes([...toDoes, text]);
+      inputRef.current.value = "";
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newToDoes = toDoes.filter((i) => i !== index);
+    setToDoes(newToDoes);
+  };
 
   return (
     <div className='container'>
-<div>
-
-      <h1 className='h1'>To Do Liste</h1>
-      <input ref={inputRef} className='input' placeholder='type some thing'/>
-      <button onClick={handlare} className='button'>Add</button>
-    <ul>
-    {toDoes.map((items)=>{
-      return (<li>{items}</li>)
-    })}
-    </ul>
-
-    </div>
+      <div>
+        <h1 className='h1'>To Do List</h1>
+        <input ref={inputRef} className='input' placeholder='type something' />
+        <button onClick={handleAdd} className='button'>Add</button>
+        <ul>
+          {toDoes.map((item, index) => (
+            <li key={index} onClick={() => handleDelete(index)}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
